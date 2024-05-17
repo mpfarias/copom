e982b9258f2fa80ac1731d406ec022dadfeeff93
Marcelo Farias <mpfarias85@gmail.com>
2024-05-16 23:31:39 -0300

    Update home.jsx

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,9 +1,5 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import {CardActionArea, CardActions } from '@mui/material';
+
 
 export default function Home() {
   return (
