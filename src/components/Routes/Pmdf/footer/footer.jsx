51520ce8e5771bebea7b7ac5dc068b55ef8b2846
Marcelo Farias <mpfarias85@gmail.com>
2024-07-29 17:40:59 -0300

    Update

--- src/components/Routes/Pmdf/footer/footer.jsx
+++ src/components/Routes/Pmdf/footer/footer.jsx
@@ -1,14 +1,15 @@
 import Sheet from '@mui/joy/Sheet';
 import Stack from '@mui/joy/Stack';
-import Box from '@mui/joy/Box';
+import {Box, Grid} from '@mui/material';
+
 import { styled } from '@mui/joy/styles';
 
 const Item = styled(Sheet)(({ theme }) => ({
     bottom: 0,
-    paddingTop: 5,
+    paddingTop: 8,
     paddingBottom: 0,
     width: '100%',
-    height: '1.5rem',
+    height: '2rem',
     backgroundColor: '#346399',
     textAlign: 'center',
     fontWeight: theme.fontWeight.md,
@@ -19,17 +20,41 @@ const Item = styled(Sheet)(({ theme }) => ({
 
 export default function Footer() {
     return (
-
-        <Box sx={{ width: '100%', marginBottom: 0 }}>
-            <Stack spacing={0}>
-                <Item>Desenvolvido por:
-                    <Item>2º Sgt Marcelo Pires de Farias - (61) 9 9972-9293
-                        <Item>2º Sgt Rafael Gadelha de Menezes - (61) 9 9277-1680
+        <Grid container sx={{ flex: 1}}>
+            <Grid item xs={12} md={4}>
+                <Box sx={{ width: '100%', marginBottom: 0 }}>
+                    <Stack spacing={0}>
+                        <Item>Polícia Militar do Distrito Federal
+                            <Item></Item>
+                                <Item></Item>
                         </Item>
-                    </Item>
-                </Item>
-            </Stack>
-        </Box>
+                    </Stack>
+                </Box>
+            </Grid>
+            <Grid item md={4}>
+                <Box sx={{ width: '100%', marginBottom: 0 }}>
+                    <Stack spacing={0}>
+                        <Item>HEFESTO - Sistema de Criação e Despacho de Ocorrências
+                            <Item></Item>
+                                <Item>
+                            </Item>
+                        </Item>
+                    </Stack>
+                </Box>
+            </Grid>
+            <Grid item xs={12} md={4}>
+                <Box sx={{ width: '100%', marginBottom: 0 }}>
+                    <Stack spacing={0}>
+                        <Item>Desenvolvido por:
+                            <Item>2º Sgt Marcelo Pires de Farias - (61) 9 9972-9293
+                                <Item>2º Sgt Rafael Gadelha de Menezes - (61) 9 9277-1680
+                                </Item>
+                            </Item>
+                        </Item>
+                    </Stack>
+                </Box>
+            </Grid>
+        </Grid>
     )
 }
 
